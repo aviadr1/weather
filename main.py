fcd500806eb397855de3d6a138fb9597f8b58c23
Aviad Rozenhek <aviadr1@gmail.com>
2024-12-22 19:53:43 +0200

    demo seaborn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.markdown("""
 
@@ -13,3 +15,10 @@ st.markdown("""
 """)
 
 st.radio("Which dessert is best?", ["Cake", "Ice cream", "Pie"])
+
+df = sns.load_dataset("penguins")
+
+fig, ax = plt.subplots()  # Create a new figure, get the axes object
+sns.scatterplot(data=df, x="flipper_length_mm", y="bill_length_mm", hue="species"
+                , ax=ax)  # Use the axes object to plot on the same figure
+st.pyplot(fig)  # Show the figure in Streamlit
