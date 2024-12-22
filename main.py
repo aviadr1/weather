8c7cf5e6de40ab8494a3bfea7e88bfcf8c7c7ea1
Aviad Rozenhek <aviadr1@gmail.com>
2024-12-22 20:07:41 +0200

    plotly and seaborn demo

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly.express as px
 
-st.markdown("""
-
-# Title
-## Subtitle
-
-- bullet 1
-- bullet 2
-- bullet 3
-
-> Amazing Quote
-""")
-
-st.radio("Which dessert is best?", ["Cake", "Ice cream", "Pie"])
-
+# Load the dataset
 df = sns.load_dataset("penguins")
 
-fig, ax = plt.subplots()  # Create a new figure, get the axes object
-sns.scatterplot(data=df, x="flipper_length_mm", y="bill_length_mm", hue="species"
-                , ax=ax)  # Use the axes object to plot on the same figure
-st.pyplot(fig)  # Show the figure in Streamlit
+# Sidebar for plot selection
+plot_choice = st.radio("Choose your plot library:", ["Seaborn", "Plotly"])
+
+if plot_choice == "Seaborn":
+    # Seaborn plot
+    fig, ax = plt.subplots()
+    sns.scatterplot(data=df, x="flipper_length_mm", y="bill_length_mm", hue="species", ax=ax)
+    st.pyplot(fig)
+elif plot_choice == "Plotly":
+    # Plotly plot
+    fig = px.scatter(
+        df,
+        x="flipper_length_mm",
+        y="bill_length_mm",
+        color="species",
+        title="Penguins: Flipper Length vs Bill Length",
+    )
+    st.plotly_chart(fig)
+
